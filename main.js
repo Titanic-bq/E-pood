d9de690c02b28687f5fe2e7e8fce213dee54bd4d
hendrik <hendrik>
2025-11-29 15:13:25 +0200

    teksti järgi 1.

---
+++ main.js
@@ -0,0 +1,21 @@
+class Product {
+  constructor(id, title, price, category) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.category = category;
+  }
+
+  describe() {
+    return `${this.title} (${this.category}) - price: ${this.price} €`;
+  }
+
+  static discountedPrice(price, discountPercent) {
+    return price - price * (discountPercent / 100);
+  }
+}
+
+const laptop = new Product(1, "Sülearvuti", 999.99, "Elektroonika");
+
+console.log(laptop.describe());
+console.log(Product.discountedPrice(laptop.price, 10));
