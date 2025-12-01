2d59ae2b2bda31a38b7496d30c77f9630c4ce208
hendrik <hendrik>
2025-12-01 13:12:34 +0200

    order

--- main.js
+++ main.js
@@ -59,7 +59,22 @@ console.log(cart.totalItems);
 //Order klass
 
 class Order {
-    constructor(cart) {
-        this.orderDate = new Date();
-        this.cart = cart;
-    }
+  constructor(cart) {
+    this.orderDate = new Date();
+    this.cart = cart;
+  }
+  printOrder() {
+    console.log("Tellimuse kuupäev:", this.orderDate.toLocaleString());
+    console.log("Tooted:");
+
+    this.cart.items.forEach((item) => {
+      console.log(
+        `- ${item.product.title} x ${item.quantity} = ${
+          item.product.price * item.quantity
+        }€`
+      );
+    });
+
+    console.log("Kogusumma:", this.cart.calculateTotal() + "€");
+  }
+}
