a9d857e921ec9c57afc768abad7df6427f2096f3
hendrik <hendrik>
2026-01-06 21:37:56 +0200

    mõned muudatused mainis, peaks olema korras

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-import { Product } from "./constructors/product.js";
 import { Cart } from "./constructors/Cart.js";
 import { Customer } from "./constructors/Customer.js";
 import { fetchProducts } from "./data.js";
 import { navigate } from "./router.js";
 
-const products = [
-  new Product(1, "Sülearvuti", 45.99, "Elektroonika"),
-  new Product(2, "Hiirepadi", 5.99, "Elektroonika"),
-  new Product(3, "Kohvimasin", 89.99, "Köök"),
-  new Product(4, "Raamat: JavaScript", 15.49, "Raamatud"),
-  new Product(5, "Jalgratas", 120.0, "Sport"),
-  new Product(6, "T-särk", 9.99, "Riided"),
-  new Product(7, "Kõrvaklapid", 25.0, "Elektroonika"),
-];
-
+export const cart = new Cart();
 export const customer = new Customer();
+export let products = [];
 
 const cartConstructor = new Cart();
 cartConstructor.addproduct(products[0], 2);
