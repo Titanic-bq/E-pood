3d9c2444e40004c47de07df0202dc448657c74fc
Hendrik <hendrik.jurgenson@voco.ee>
2026-01-14 11:16:34 +0200

    stiilid

--- api.js
+++ api.js
@@ -2,28 +2,61 @@ import { Product } from "./constructors/product.js";
 
 export const getProductsDataFromJson = async () => {
   try {
-    const response = await fetch("/api/products");
-    const jsondata = await response.json();
-    console.log("Products from backend:", jsondata);
-    const constructedData = jsondata.map(
-      (product) =>
+    const data = await fetch("./data.json");
+    return data.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getProductsByCategory = async (category) => {
+  try {
+    const data = await fetch(`${BASE_URL}/products/category/${category}`);
+    const productsData = await data.json();
+
+    const dataObject = productsData.map(
+      (item) =>
         new Product(
-          product.id,
-          product.title || product.nimi,
-          product.price || product.hind,
-          product.category || product.kategooria
+          item.id,
+          item.title,
+          item.price,
+          item.category,
+          item.description,
+          item.image
         )
     );
-    console.log("Constructed products:", constructedData);
-    return constructedData;
+    return dataObject;
   } catch (error) {
-    console.error("Error fetching products:", error);
+    console.error(error);
   }
 };
 export const getAllCategory = async () => {
   try {
     const data = await fetch(`${BASE_URL}/products/categories`);
     return data.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getProductById = async (productId) => {
+  console.log("id", productId);
+  try {
+    const data = await fetch(`${BASE_URL}/products/${productId}`);
+
+    const productData = await data.json();
+
+    console.log(productData);
+
+    const dataObject = new Product(
+      productData.id,
+      productData.title,
+      productData.price,
+      productData.category,
+      productData.description,
+      productData.image
+    );
+
+    return dataObject;
   } catch (error) {
     console.error(error);
   }
